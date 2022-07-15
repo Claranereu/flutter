b01b24d995e30df4a4ac52f4a7f559d2e162a78c
claranereu <nereuclara@gmail.com>
2022-07-15 12:01:23 -0300

    add um item a lista

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,18 @@ class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
 
   @override
- _MyAppState createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String>listaToDo = [];
+  List<String> listaToDo = [];
 
   @override
   Widget build(BuildContext context) {
     listaToDo.add("1");
     listaToDo.add("2");
     listaToDo.add("3");
+    listaToDo.add("4");
     print(listaToDo.length);
-  
   }
 }
