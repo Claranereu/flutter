6494b6467c79db661134142bd7c03a940fafd1ce
claranereu <nereuclara@gmail.com>
2022-07-15 11:40:59 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+ _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String>listaToDo = [];
+
+  @override
+  Widget build(BuildContext context) {
+    listaToDo.add("1");
+    listaToDo.add("2");
+    listaToDo.add("3");
+    print(listaToDo.length);
+  
+  }
+}
